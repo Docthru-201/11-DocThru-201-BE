7f3037d36cbe400d3f54706e309f1c07e506d2f9
DevWoojin97 <dnekdnwls@gmail.com>
2026-03-12 14:25:56 +0900

    feat: auth, users, profiles dto 생성

--- src/controllers/auth/dto/auth.dto.js
+++ src/controllers/auth/dto/auth.dto.js
@@ -1,16 +1,57 @@
 import { z } from 'zod';
 
-export const signUpSchema = z.object({
-  email: z.email('유효한 이메일 형식이 아닙니다.'),
-  password: z
-    .string({ error: '비밀번호는 필수입니다.' })
-    .min(6, '비밀번호는 6자 이상이어야 합니다.'),
-  name: z.string().min(2, '이름은 2자 이상이어야 합니다.'),
-});
+export const signupSchema = z
+  .object({
+    email: z
+      .string()
+      .trim()
+      .toLowerCase()
+      .min(1, { message: '이메일을 입력해주세요.' })
+      .email({ message: '올바른 이메일 형식이 아닙니다.' }),
 
-export const loginSchema = z.object({
-  email: z.email('유효한 이메일 형식이 아닙니다.'),
-  password: z
-    .string({ error: '비밀번호는 필수입니다.' })
-    .min(1, '비밀번호를 입력해주세요.'),
-});
+    nickname: z
+      .string()
+      .trim()
+      .min(2, { message: '닉네임은 최소 2자 이상입니다.' })
+      .max(8, { message: '닉네임은 8자 이하입니다.' })
+      .regex(/^[가-힣a-zA-Z0-9]+$/, {
+        message: '닉네임은 한글, 영문, 숫자만 사용할 수 있습니다.',
+      }),
+
+    password: z
+      .string()
+      .min(1, { message: '비밀번호를 입력해주세요.' })
+      .min(8, { message: '비밀번호는 최소 8자 이상이어야 합니다.' })
+      .max(15, { message: '비밀번호는 15자 이하로 입력해주세요.' })
+      .regex(/[A-Za-z]/, {
+        message: '비밀번호에 영문자를 포함해주세요.',
+      })
+      .regex(/[0-9]/, {
+        message: '비밀번호에 숫자를 포함해주세요.',
+      })
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, {
+        message: '비밀번호에 특수문자를 포함해주세요.',
+      }),
+    confirmPassword: z
+      .string()
+      .min(1, { message: '비밀번호 확인을 입력해주세요.' }),
+  })
+  .strict()
+
+  .refine((data) => data.password === data.confirmPassword, {
+    message: '비밀번호가 일치하지 않습니다.',
+    path: ['confirmPassword'],
+  });
+
+export const loginSchema = z
+  .object({
+    email: z
+      .string()
+      .trim()
+      .toLowerCase()
+      .min(1, { message: '이메일을 입력해주세요.' })
+      .email({ message: '올바른 이메일 형식이 아닙니다.' }),
+
+    password: z.string().min(1, { message: '비밀번호를 입력해주세요.' }),
+  })
+  .strict();
