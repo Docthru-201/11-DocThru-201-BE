8267cd69951a7831e46343c4685c3fdbe1513e2e
yyejin00 <bywlslgnsl20@gmail.com>
2026-03-14 12:12:18 +0900

    feat: work,commet zod 작성 중 (#18)

--- src/controllers/comments/dto/comment.dto.js
+++ src/controllers/comments/dto/comment.dto.js
@@ -1,6 +1,22 @@
 import { z } from 'zod';
 
-export const workIdParamSchema = z.object({});
-export const commentIdParamSchema = z.object({});
-export const createCommentSchema = z.object({});
-export const updateCommentSchema = z.object({});
+export const workIdParamSchema = z.object({
+  workId: z.string({
+    required_error: '잘못된 접근입니다. workID 정보가 누락되었습니다.',
+  }),
+});
+export const commentIdParamSchema = z.object({
+  workId: z.string({
+    required_error: '잘못된 접근입니다. 댓글ID 정보가 필요합니다.',
+  }),
+});
+export const createCommentSchema = z.object({
+  workId: z.string({
+    required_error: '잘못된 접근입니다. workID 정보가 누락되었습니다.',
+  }),
+  content: z.string().min(1, '댓글 내용을 작성해주세요.'),
+  parentId: z.string().optional(), //대댓글을 위한 부모댓글 id
+});
+export const updateCommentSchema = z.object({
+  content: z.string().min(1, '수정할 댓글을 작성해주세요').optional(),
+});
--- src/controllers/works/dto/work.dto.js
+++ src/controllers/works/dto/work.dto.js
@@ -1,5 +1,14 @@
 import { z } from 'zod';
 
-export const workIdParamSchema = z.object({});
-export const createWorkSchema = z.object({});
-export const updateWorkSchema = z.object({});
+export const workIdParamSchema = z.object({
+  id: z.string().min(1, '유효한 작업물 ID가 필요합니다.'), // ULID는 문자열
+});
+export const createWorkSchema = z.object({
+  challengId: z.string({
+    required_error: '잘못된 접근입니다. 챌린지 정보가 누락되었습니다.',
+  }),
+  content: z.string().min(1, '작업물의 내용을 작성해주세요.'),
+});
+export const updateWorkSchema = z.object({
+  content: z.string().min(1, '수정할 본문 내용을 입력해주세요.').optional(),
+});
