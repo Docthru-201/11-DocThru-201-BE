bcb99385ac7fe52147087755f5c79679268a89d9
DevWoojin97 <dnekdnwls@gmail.com>
2026-03-16 15:28:20 +0900

    feat: 유저 컨트롤러 재생성 및 import 경로 수정

--- src/controllers/users/user.controller.js
+++ src/controllers/users/user.controller.js
@@ -1,25 +1,61 @@
 import { BaseController } from '#controllers/base.controller.js';
+import { HTTP_STATUS } from '#constants';
+import { validate, needsLogin } from '#middlewares';
+import { updateUserSchema } from './dto/user.dto.js';
+import { z } from 'zod';
+
+const idParamSchema = z.object({
+  id: z.string().uuid(),
+});
 
 export class UsersController extends BaseController {
   #usersService;
+  #profilesController;
 
-  constructor({ usersService }) {
+  constructor({ usersService, profilesController }) {
     super();
     this.#usersService = usersService;
+    this.#profilesController = profilesController;
   }
 
   routes() {
+    this.router.get('/me', needsLogin, (req, res) => this.getMe(req, res));
+    this.router.patch(
+      '/me',
+      needsLogin,
+      validate('body', updateUserSchema),
+      (req, res) => this.updateMe(req, res),
+    );
+    this.router.delete('/me', needsLogin, (req, res) =>
+      this.deleteMe(req, res),
+    );
+    this.router.get('/:id', validate('params', idParamSchema), (req, res) =>
+      this.getUserById(req, res),
+    );
+    this.router.use('/', this.#profilesController.routes());
     return this.router;
   }
 
-  async findAll(req, res) {}
+  async getMe(req, res) {
+    const user = await this.#usersService.getUserById(req.user.id);
+    res.status(HTTP_STATUS.OK).json(user);
+  }
 
-  async findById(req, res) {}
+  async updateMe(req, res) {
+    const updatedUser = await this.#usersService.updateUser(
+      req.user.id,
+      req.body,
+    );
+    res.status(HTTP_STATUS.OK).json(updatedUser);
+  }
 
-  async create(req, res) {}
+  async deleteMe(req, res) {
+    await this.#usersService.deleteUser(req.user.id);
+    res.sendStatus(HTTP_STATUS.NO_CONTENT);
+  }
 
-  async update(req, res) {}
-
-  async delete(req, res) {}
+  async getUserById(req, res) {
+    const user = await this.#usersService.getPublicProfile(req.params.id);
+    res.status(HTTP_STATUS.OK).json(user);
+  }
 }
-
