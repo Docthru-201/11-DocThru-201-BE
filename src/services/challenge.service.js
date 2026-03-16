92ccdfa79500fbf9c6772a2e03b38d3df030c765
KIMHYUNJE <ranflir.sc@gmail.com>
2026-03-16 23:18:40 +0900

    feat: 챌린지 서비스 비지니스 로직 작성 (#23)

--- src/services/challenge.service.js
+++ src/services/challenge.service.js
@@ -5,15 +5,48 @@ export class ChallengesService {
     this.#challengeRepository = challengeRepository;
   }
 
-  async listChallenges() {}
+  async listChallenges(query) {
+    return await this.#challengeRepository.findMany(query);
+  }
 
-  async getChallengeDetail() {}
+  async getChallengeDetail(id) {
+    const challenge = await this.#challengeRepository.findById(id);
+    if (!challenge) {
+      throw new Error(`There's no challenge at all.`);
+    }
+    return challenge;
+  }
 
-  async createChallenge() {}
+  async createChallenge(data) {
+    return await this.#challengeRepository.create(data);
+  }
 
-  async updateChallenge() {}
+  async updateChallenge(id, userId, updateData) {
+    const challenge = await this.#challengeRepository.findById(id);
 
-  async deleteChallenge() {}
+    if (!challenge) {
+      throw new Error(`There's no challenge at all.`);
+    }
+    if (challenge.authorId !== userId) {
+      throw new Error(`There's no authority to update at all.`);
+    }
+    return await this.#challengeRepository.update(id, updateData);
+  }
 
-  async getChallengeByUser() {}
+  async deleteChallenge(id, userId) {
+    const challenge = await this.#challengeRepository.findById(id);
+
+    if (!challenge) {
+      throw new Error(`There's no challenge at all.`);
+    }
+    if (challenge.authorId !== userId) {
+      throw new Error(`There's no authority to delete at all.`);
+    }
+
+    await this.#challengeRepository.delete(id);
+  }
+
+  async getChallengesByUser(userId) {
+    return await this.#challengeRepository.findByUserId(userId);
+  }
 }
