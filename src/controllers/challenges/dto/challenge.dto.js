b7ef34ae70224a7882b12603e1166725bb3a9f8d
KIMHYUNJE <ranflir.sc@gmail.com>
2026-03-16 18:12:07 +0900

    feat: zod스키마 리뷰 반영 (#18)

--- src/controllers/challenges/dto/challenge.dto.js
+++ src/controllers/challenges/dto/challenge.dto.js
@@ -1,10 +1,12 @@
 import { z } from 'zod';
+import { Category, ChallengeStatus, Type } from '#generated/prisma/enums.js';
 
-const typeEnum = z.enum(['NEXT_JS', 'API', 'CAREER', 'MODERN_JS', 'WEB']);
-const categoryEnum = z.enum(['DOCUMENT', 'BLOG']);
-const statusEnum = z.enum(['PENDING', 'APPROVAL', 'CLOSED', 'DELETED'], {
-  error: '유효한 상태 값이 아닙니다.',
-});
+//규량st
+// const typeEnum = z.enum(['NEXT_JS', 'API', 'CAREER', 'MODERN_JS', 'WEB']);
+// const categoryEnum = z.enum(['DOCUMENT', 'BLOG']);
+// const statusEnum = z.enum(['PENDING', 'APPROVAL', 'CLOSED', 'DELETED'], {
+//   error: '유효한 상태 값이 아닙니다.',
+// });
 const DESCRIPTION_MIN_LENGTH = 10;
 const DESCRIPTION_MAX_LIMIT = 500;
 const TITLE_MAX_LENGTH = 100;
@@ -15,10 +17,8 @@ const PARTICIPANT_MIN_LIMIT = 1;
 // export const listChallengesQuerySchema = z.object({}); // TODO: 추후 페이지네이션 추가
 
 //나도 ⭐️수전전
-export const ulidSchema = z
-  .string()
-  .trim()
-  .ulid({ message: `유효한 id 형식(ULID)이 아닙니다.` });
+export const ulidSchema = z.string().trim();
+z.ulid({ message: `유효한 id 형식(ULID)이 아닙니다.` });
 
 export const challengeIdParamSchema = z.object({
   id: ulidSchema,
@@ -33,10 +33,11 @@ export const createChallengeSchema = z.object({
     )
     .max(TITLE_MAX_LENGTH, `제목은 ${TITLE_MAX_LENGTH}자를 넘을 수 없습니다.`), // 101자 에러
   originalUrl: z.url(),
-  type: typeEnum,
-  category: categoryEnum,
+  type: z.enum(Type, { required_error: `타입을 선택해 주세요.` }),
+  category: z.enum(Category, { required_error: `카테고리를 선택해 주세요.` }),
   description: z
     .string()
+    .trim()
     .min(
       DESCRIPTION_MIN_LENGTH,
       `챌린지 설명은 최소 ${DESCRIPTION_MIN_LENGTH}자 이상 상세히 적어주세요.`,
@@ -45,7 +46,7 @@ export const createChallengeSchema = z.object({
       DESCRIPTION_MAX_LIMIT,
       `설명은 ${DESCRIPTION_MAX_LIMIT}자를 넘길 수 없습니다.`,
     ),
-  deadline: z.string().trim().datetime(),
+  deadline: z.string().trim().iso.datetime(),
   maxParticipants: z
     .number()
     .int(`참가자 수는 정수로 입력해주세요.`)
@@ -65,8 +66,8 @@ export const updateChallengeSchema = z.object({
     )
     .max(TITLE_MAX_LENGTH, `제목은 ${TITLE_MAX_LENGTH}자를 넘을 수 없습니다.`),
   originalUrl: z.url(`올바른 웹 주소 형식이 아닙니다.`), // http://, https:// 포함
-  type: typeEnum,
-  category: categoryEnum,
+  type: z.enum(Type, { required_error: `타입을 선택해 주세요.` }),
+  category: z.enum(Category, { required_error: `카테고리를 선택해 주세요.` }),
   description: z
     .string()
     .min(
@@ -77,7 +78,10 @@ export const updateChallengeSchema = z.object({
       DESCRIPTION_MAX_LIMIT,
       `설명은 ${DESCRIPTION_MAX_LIMIT}자를 넘길 수 없습니다.`,
     ),
-  deadline: z.string().trim().datetime(`올바른 날짜와 시간 형식이 아닙니다.`),
+  deadline: z
+    .string()
+    .trim()
+    .iso.datetime(`올바른 날짜와 시간 형식이 아닙니다.`),
   maxParticipants: z
     .number()
     .int(`참가자 수는 정수로 입력해주세요.`)
@@ -85,13 +89,13 @@ export const updateChallengeSchema = z.object({
       PARTICIPANT_MIN_LIMIT,
       `참가자는 최소 ${PARTICIPANT_MIN_LIMIT}명 이상이어야 합니다.`,
     ),
-  status: statusEnum,
+  status: z.enum(ChallengeStatus),
   declineReason: z
     .string()
     .trim()
     .max(
       REASON_MAX_LIMIT,
-      `거절 사유가 너무 길고 장황합니다. ${REASON_MAX_LIMIT}자를 넘기지 마세요.`,
+      `거절 사유가 너무 깁니다. ${REASON_MAX_LIMIT}자를 넘기지 마세요.`,
     )
     .optional(),
 });
