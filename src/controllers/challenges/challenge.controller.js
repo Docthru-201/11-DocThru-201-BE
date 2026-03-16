d9696614db937ea9f2702fe2e461c4add4c030d4
KIMHYUNJE <ranflir.sc@gmail.com>
2026-03-16 16:20:57 +0900

    feat: 챌린지 전체 목록 조회 컨트롤러 구현 (#23)

--- src/controllers/challenges/challenge.controller.js
+++ src/controllers/challenges/challenge.controller.js
@@ -1,4 +1,11 @@
 import { BaseController } from '#controllers/base.controller.js';
+import { HTTP_STATUS } from '#constants';
+import { validate, needsLogin } from '#middlewares';
+import {
+  createChallengeSchema,
+  idParamSchema,
+  updateChallengeSchema,
+} from './dto/challenge.dto.js';
 
 export class ChallengesController extends BaseController {
   #challengesService;
@@ -9,12 +16,47 @@ export class ChallengesController extends BaseController {
   }
 
   routes() {
+    this.router.get('/me', needsLogin, (req, res) =>
+      this.getMyChallenges(req, res),
+    );
+
+    this.router.get('/', (req, res) => this.findAll(req, res));
+
+    this.router.post(
+      '/',
+      needsLogin,
+      validate('body', createChallengeSchema),
+      (req, res) => this.create(req, res),
+    );
+
+    this.router.patch(
+      '/:id',
+      needsLogin,
+      validate('params', idParamSchema),
+      validate('body', updateChallengeSchema),
+      (req, res) => this.update(req, res),
+    );
+
+    this.router.delete(
+      '/:id',
+      needsLogin,
+      validate('params', idParamSchema),
+      (req, res) => this.delete(req, res),
+    );
+
     return this.router;
   }
 
-  async findAll(req, res) {}
+  async findAll(req, res) {
+    const challenges = await this.#challengesService.listChallenges(req.query);
+    res.status(HTTP_STATUS.OK).json(challenges);
+  }
 
-  async findById(req, res) {}
+  async findById(req, res) {
+    const { id } = req.params;
+    const challenge = await this.#challengesService.getChallengeDetail(id);
+    res.status(HTTP_STATUS.OK).json(challenge);
+  }
 
   async create(req, res) {}
 
