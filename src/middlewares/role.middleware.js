b3d872ebc30ee7204f0bae36d670da7bca9e39d3
DevWoojin97 <dnekdnwls@gmail.com>
2026-03-17 13:36:02 +0900

    refactor: role.middleware.js 파일에서 불필요한 로직 삭제  (#10)

--- src/middlewares/role.middleware.js
+++ src/middlewares/role.middleware.js
@@ -17,20 +17,3 @@ export function adminValidator(req, res, next) {
 
   next();
 }
-
-/**
- * User 권한 검증 미들웨어
- */
-export function userValidator(req, res, next) {
-  const role = req.user?.role;
-
-  if (!role) {
-    return res.status(401).json({ message: '로그인이 필요합니다.' });
-  }
-
-  if (role !== 'USER') {
-    return res.status(403).json({ message: '권한이 없습니다.' });
-  }
-
-  next();
-}
