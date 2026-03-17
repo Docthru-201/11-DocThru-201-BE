9a9de49cfa4698c546de80392220ddf95c2a8f8e
lareina7486 <lareina7486@gmail.com>
2026-03-17 16:27:18 +0900

    feat: controller 수정 (#00)

--- src/controllers/challenges/challenge.controller.js
+++ src/controllers/challenges/challenge.controller.js
@@ -1,4 +1,11 @@
 import { BaseController } from '#controllers/base.controller.js';
+import { HTTP_STATUS } from '#constants';
+import { validate, needsLogin } from '#middlewares';
+import {
+  createChallengeSchema,
+  idParamSchema,
+  updateChallengeSchema,
+} from './dto/challenge.dto.js';
 
 export class ChallengesController extends BaseController {
   #challengesService;
@@ -9,6 +16,38 @@ export class ChallengesController extends BaseController {
   }
 
   routes() {
+    this.router.get('/me', needsLogin, (req, res) =>
+      this.getMyChallenges(req, res),
+    );
+
+    this.router.get('/', (req, res) => this.findAll(req, res));
+
+    this.router.get('/:id', validate('params', idParamSchema), (req, res) =>
+      this.findById(req, res),
+    );
+
+    this.router.post(
+      '/',
+      needsLogin,
+      validate('body', createChallengeSchema),
+      (req, res) => this.create(req, res),
+    );
+
+    this.router.patch(
+      '/:id',
+      needsLogin,
+      validate('params', idParamSchema),
+      validate('body', updateChallengeSchema),
+      (req, res) => this.update(req, res),
+    );
+
+    this.router.delete(
+      '/:id',
+      needsLogin,
+      validate('params', idParamSchema),
+      (req, res) => this.delete(req, res),
+    );
+
     return this.router;
   }
 
@@ -21,17 +60,54 @@ export class ChallengesController extends BaseController {
 
   // res <- controller req <-> service <-> repository <-> DB
 
-  async findAll(req, res) {}
+  async findAll(req, res) {
+    const challenges = await this.#challengesService.listChallenges(req.query);
+    res.status(HTTP_STATUS.OK).json(challenges);
+  }
 
-  async findById(req, res) {}
+  async findById(req, res) {
+    const { id } = req.params;
+    const challenge = await this.#challengesService.getChallengeDetail(id);
+    res.status(HTTP_STATUS.OK).json(challenge);
+  }
 
-  async create(req, res) {}
+  async create(req, res) {
+    const challengeData = req.body;
+    const userId = req.user.id;
 
-  async update(req, res) {}
+    const newChallenge = await this.#challengesService.createChallenge({
+      ...challengeData,
+      authorId: userId,
+    });
+    res.status(HTTP_STATUS.CREATED).json(newChallenge);
+  }
 
-  async delete(req, res) {}
+  async update(req, res) {
+    const { id } = req.params;
+    const updataData = req.body;
+    const userId = req.user.id;
 
-  async getMyChallenges(req, res) {}
+    const updateChallenge = await this.#challengesService.updateChallenge(
+      id,
+      userId,
+      updataData,
+    );
+    res.status(HTTP_STATUS.OK).json(updateChallenge);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const userId = req.user.id;
+
+    await this.#challengesService.deleteChallenge(id, userId);
+    res.sendStatus(HTTP_STATUS.NO_CONTENT);
+  }
+
+  async getMyChallenges(req, res) {
+    const userId = req.user.id;
+
+    const myChallenges =
+      await this.#challengesService.getChallengesByUser(userId); //challenge.service.js에 없는데, 일단 넣어둠
+    res.status(HTTP_STATUS.OK).json(myChallenges);
+  }
 }
-
-//
